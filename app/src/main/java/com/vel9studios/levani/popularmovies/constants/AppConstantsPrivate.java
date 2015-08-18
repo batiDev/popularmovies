191619ac27d2efb1d9a1372f974269594c99fd7c
batiDev <levandev9053@gmail.com>
2015-08-18 19:21:40 -0400

    Create AppConstantsPrivate.java

---
+++ app/src/main/java/com/vel9studios/levani/popularmovies/constants/AppConstantsPrivate.java
@@ -0,0 +1,10 @@
+package com.vel9studios.levani.popularmovies.constants;
+
+/**
+ * Private Constants
+ */
+public class AppConstantsPrivate {
+
+    //Make sure to fill this out with your own API key
+    public static final String API_KEY= "";
+}
